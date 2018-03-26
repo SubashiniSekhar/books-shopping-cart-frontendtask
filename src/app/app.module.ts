1904d605eedf2f64c55e8ea2e4b93dba45614ea8
SubashiniSekhar <subashini.sekhar@gmail.com>
2018-03-26 13:34:23 +0200

    removed unneeded component from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BooksHomepageComponent } from './books-homepage/books-homepage.componen
 import { BooksComponent } from './books/books.component';
 import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
 import { BookComponent } from './books/book/book.component';
-import { CartComponent } from './cart/cart.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import {AppRouting} from "./app-routes";
 import { AddToCartComponent } from './books/add-to-cart/add-to-cart.component';
@@ -23,7 +22,6 @@ import { ShoppingBasketComponent } from './shopping-basket/shopping-basket.compo
     BooksComponent,
     CheckoutPageComponent,
     BookComponent,
-    CartComponent,
     ErrorPageComponent,
     AddToCartComponent,
     ShoppingBasketComponent
